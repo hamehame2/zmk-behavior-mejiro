ba8c3fb52611acaa9a89d3fa67922ea1614a051e
hamehame2 <38934289+hamehame2@users.noreply.github.com>
2026-02-08 23:35:00 +0900

    Update behavior_naginata.c

--- src/behaviors/behavior_naginata.c
+++ src/behaviors/behavior_naginata.c
@@ -101,15 +101,27 @@ static inline void tap_key(uint32_t keycode) {
     raise_zmk_keycode_state_changed_from_encoded(keycode, false, timestamp);
 }
 
-static void tap_ascii_letter(char c) {
-    if (c >= 'a' && c <= 'z') c = (char)(c - 'a' + 'A');
-    if (c < 'A' || c > 'Z') return;
-    tap_key((uint32_t)c);
+static uint32_t keycode_from_ascii_letter(char c) {
+    if (c >= 'a' && c <= 'z') {
+        c = (char)(c - 'a' + 'A');
+    }
+    switch (c) {
+    case 'A': return A; case 'B': return B; case 'C': return C; case 'D': return D; case 'E': return E;
+    case 'F': return F; case 'G': return G; case 'H': return H; case 'I': return I; case 'J': return J;
+    case 'K': return K; case 'L': return L; case 'M': return M; case 'N': return N; case 'O': return O;
+    case 'P': return P; case 'Q': return Q; case 'R': return R; case 'S': return S; case 'T': return T;
+    case 'U': return U; case 'V': return V; case 'W': return W; case 'X': return X; case 'Y': return Y;
+    case 'Z': return Z;
+    default:  return NONE;
+    }
 }
 
 static void send_stroke_debug_letters(const char *s) {
     for (const char *p = s; *p; p++) {
-        tap_ascii_letter(*p);
+        uint32_t kc = keycode_from_ascii_letter(*p);
+        if (kc != NONE) {
+            tap_key(kc);
+        }
     }
 }
 
