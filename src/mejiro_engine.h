0af113795d5a71209d589bd27969d0b0d14b3c35
hamehame2 <38934289+hamehame2@users.noreply.github.com>
2026-02-08 22:29:57 +0900

    Delete src directory

--- src/mejiro_engine.h
+++
@@ -1,9 +0,0 @@
-/* SPDX-License-Identifier: GPL-3.0-or-later */
-#pragma once
-#include <stdint.h>
-
-void mj_engine_reset(void);
-
-/* key_id: 0..23 from dt-bindings (lS..rX) */
-void mj_engine_press(uint8_t key_id);
-void mj_engine_release(uint8_t key_id);
